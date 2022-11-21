461c8d68e069ef3d9c201b5aa7a0cf68e54b1bf7
Davyd Almeida <almeidadavyd@outlook.com>
2022-11-20 21:04:06 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APS2-Movel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

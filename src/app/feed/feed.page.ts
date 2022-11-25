21b58ac195761fe1bebedb1e2ffe9739f8b97510
Davyd Almeida <almeidadavyd@outlook.com>
2022-11-25 10:48:13 -0300

    tela feed infinito parcial com erro

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -10,7 +10,7 @@ import { RickAndMort } from '../interface/feed';
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
 })
-export class FeedPage implements OnInit {
+export class FeedPage {
   // constructor(private feed: FeedService) { }
 
   // async ngOnInit() {
@@ -23,40 +23,58 @@ export class FeedPage implements OnInit {
   private index: number = 0;
   variavel = 1000;
 
-  constructor(private httpClient: HttpClient, private feed: FeedService) {
+  constructor(private httpClient: HttpClient, private feed: FeedService) { this.loadProuct() }
 
-    for (let i = 0; i < this.variavel; i++) {
-      this.items.push(`Item ${i + 1}`)
-    }
-    this.itemsPage = this.items.slice(this.index, this.offset + this.index);
-    this.index += this.offset;
+  //   for (let i = 0; i < this.variavel; i++) {
+  //     this.items.push(`Item ${i + 1}`)
+  //   }
+  //   this.itemsPage = this.items.slice(this.index, this.offset + this.index);
+  //   this.index += this.offset;
+  // }
+
+  // loadData(event: any) {
+
+  //   setTimeout(() => {
+  //     let news = this.items.slice(this.index, this.offset + this.index);
+  //     this.index += this.offset;
+
+  //     for (let i = 0; i < news.length; i++) {
+  //       this.itemsPage.push(news[i]);
+  //     }
+  //     event.target.complete();
+
+  //     if (this.itemsPage.length === this.variavel) {
+  //       event.target.disabled = true;
+  //     }
+  //   }, 800);
+
+  // }
+
+  // async ngOnInit(): Promise<void> {
+  //   let personagens = await lastValueFrom(this.feed.listarPersonagens());
+  //   console.log(personagens);
+  // }
+  products = [];
+  loadProuct() {
+    this.httpClient.get('https://rickandmortyapi.com/api/character').subscribe(res => {
+      console.log(res);
+      this.products = this.products.concat(res['data']);
+    })
   }
-
-  loadData(event: any) {
-
-    setTimeout(() => {
-      let news = this.items.slice(this.index, this.offset + this.index);
-      this.index += this.offset;
-
-      for (let i = 0; i < news.length; i++) {
-        this.itemsPage.push(news[i]);
-      }
-      event.target.complete();
-
-      if (this.itemsPage.length === this.variavel) {
-        event.target.disabled = true;
-      }
-    }, 800);
-
-  }
-
-  async ngOnInit(): Promise<void> {
-    let personagens = await lastValueFrom(this.feed.listarPersonagens());
-    console.log(personagens);
-  }
-
-
 
 }
+
+
+
+  //
+
+  // loadProuct(){
+  //   this.httpClient.get('https://rickandmortyapi.com/api/character').subscribe(res => {
+  //     this.products = this.products.concat(res['data']);
+  //   })
+  // }
+
+
+
 
 
